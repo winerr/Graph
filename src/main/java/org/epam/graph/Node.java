1a9aa857a5b25e1e15e46a5e0e75d23213e9d24d
Volodymyr <momotvolodymyr@gmail.com>
2018-03-12 22:14:09 +0200

    added getters to Node and toString method for GraphImpl

--- src/main/java/org/epam/graph/Node.java
+++ src/main/java/org/epam/graph/Node.java
@@ -3,42 +3,54 @@ package org.epam.graph;
 import java.util.List;
 
 public class Node {
-    private List<Integer> in;
-    private List<Integer> out;
+	private List<Integer> in;
+	private List<Integer> out;
 
+	/**
+	 * Конструктор для заповнення звязків
+	 * вузла
+	 * 
+	 * @param in
+	 *            індекси вузлів які мають шляхи до
+	 *            вузла
+	 * @param out
+	 *            індекси вузлів до яких йдуть
+	 *            шляхи від конкретного вузла
+	 */
+	public Node(List<Integer> in, List<Integer> out) {
+		this.in = in;
+		this.out = out;
+	}
 
-    /**
-     * Конструктор для заповнення звязків вузла
-     * @param in індекси вузлів які мають шляхи до вузла
-     * @param out індекси вузлів до яких йдуть шляхи від конкретного вузла
-     */
-    public Node(List<Integer> in, List<Integer> out) {
-        this.in = in;
-        this.out = out;
-    }
+	public boolean addIn(int index) {
+		return in.add(index);
+	}
 
-    public boolean addIn(int index){
-        return in.add(index);
-    }
+	public boolean addOut(int index) {
+		return in.add(index);
+	}
 
-    public boolean addOut(int index){
-        return in.add(index);
-    }
+	public boolean removeIn(int index) {
+		return in.remove(in.get(index));
+	}
 
-    public boolean removeIn(int index){
-    	return in.remove(in.get(index));
-    }
+	public boolean removeOut(int index) {
+		return out.remove(out.get(index));
+	}
 
-    public boolean removeOut(int index){
-    	return out.remove(out.get(index));
-    }
+	public int getInByIndex(int index) {
+		return in.get(index);
+	}
 
+	public int getOutByIndex(int index) {
+		return out.get(index);
+	}
 
-    public int getInByIndex(int index){
-        return in.get(index);
-    }
+	public List<Integer> getIn() {
+		return in;
+	}
 
-    public int getOutByIndex(int index){
-        return out.get(index);
-    }
+	public List<Integer> getOut() {
+		return out;
+	}
 }
